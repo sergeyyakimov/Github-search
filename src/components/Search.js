248c3158e8befd4c9423e3b2d96a7fa584ad81b2
SergeyYakimov <teriertis@yandex.ru>
2020-07-27 19:49:57 +0800

    Added usage of hooks in Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,19 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {AlertContext} from '../context/alert/AlertContext';
 
 export const Search = () => {
+  const [value, setValue] = useState('')
   const {show} = useContext(AlertContext)
 
   const onSubmit = event => {
-    if (event.key === 'Enter') {
-      show('This is alert')
+    if (event.key !== 'Enter') {
+      return
+    }
+
+    if (value.trim()) {
+      console.log('Make request with:', value)
+    } else {
+      show('Введите данные пользователя!')
     }
   }
 
@@ -17,6 +24,8 @@ export const Search = () => {
         className='form-control'
         placeholder='Введите ник пользователя...'
         onKeyPress={onSubmit}
+        value={value}
+        onChange={event => setValue(event.target.value)}
       />
     </div>
   )
